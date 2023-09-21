5e7da614416838ea95179f2a01815e38bd9d6889
Adrián Rojas <rojasadrian00@gmail.com>
2023-09-20 23:23:38 -0600

    Agregar archivos de proyecto.

---
+++ Member. Infrastructure/MemberRepository.cs
@@ -0,0 +1,22 @@
+﻿using Member.Application;
+using System.Collections.Generic;
+
+namespace Member.Infrastructure
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public static List<Domain.Member> lstMembers = new List<Domain.Member>()
+        {
+           new Domain.Member{  Name= "Kevin De Bruyne", Position ="Midfielder" , Age= 32},
+           new Domain.Member{  Name= "Vinícius Júnior", Position ="Forward" , Age= 23},
+           new Domain.Member{  Name= "Erling Haaland", Position ="Forward" , Age= 23},
+           new Domain.Member{  Name= "Kylian Mbappé", Position ="Forward" , Age= 24},
+           new Domain.Member{  Name= "Lionel Messi", Position ="Forward" , Age= 36},
+           new Domain.Member{  Name= "Yassine Bounou", Position ="Goalkeeper" , Age= 32}
+        };
+        public List<Domain.Member> GetAllMembers()
+        {
+            return lstMembers;
+        }
+    }
+}
---
+++ Member.Domain/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace Member.Domain
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public int Age { get; set; }
+
+    }
+}
